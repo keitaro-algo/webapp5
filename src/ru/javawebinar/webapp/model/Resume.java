7ca016b147f16e32e2b2fb8f57da18bae52e7ed8
keitaro-algo <keitaro.artem@gmail.com>
2015-03-22 20:32:03 +0300

    Initial commit

---
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.webapp.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * gkislin
+ * 12.12.2014.
+ */
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Resume implements Serializable {
+    static final long serialVersionUID = 1L;
+
+
+}
